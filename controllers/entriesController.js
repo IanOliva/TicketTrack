bb0059460c1083d57cc7798af6091308c75f8a13
Angeles Licudis <angeleslicudis@gmail.com>
2024-06-30 18:52:27 -0300

    Cuadro de comentarios con express

--- controllers/entriesController.js
+++ controllers/entriesController.js
@@ -1,14 +1,21 @@
+const entries = [];
 
 const renderAboutus = (req, res) => {
-    res.render('About-us');
+    res.render('about-us', {entries});
 };
 
 const renderNewEntry = (req, res) => {
-    
+    res.render('new-entry');
 };
 
 const createNewEntry = (req, res) => {
-    
+    const newEntry = {
+        title: req.body.title,
+        content: req.body.contenido,
+        publish: new Date(),
+    };
+    entries.push(newEntry);
+    res.redirect('/');
 };
 
 module.exports = {
