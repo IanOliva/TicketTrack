6e7ccca71575707304373ac1f4c0660c414ff0a2
IanOliva <ianoliva2000@gmail.com>
2024-07-07 16:40:06 -0300

    ultimos fixes antes de presentar

--- public/js/userDashboard.js
+++ public/js/userDashboard.js
@@ -1,16 +1,3 @@
-const prioridadPorID = {
-  1: { palabra: "baja", color: 'primary' }, // Verde
-  2: { palabra: "media", color: 'warning' }, // Naranja
-  3: { palabra: "alta", color: 'danger' }, // Rojo
-};
-
-const estadoTicketID = {
-  1: { palabra: "En Espera", color: "#808080" }, // Gris
-  2: { palabra: "Resuelto", color: "#008000" }, // Verde
-  3: { palabra: "Cancelado", color: "#ff0000" }, // Naranja
-  4: { palabra: "Terminado", color: "#00e7ff" }, // Cian
-};
-
 
 document.addEventListener("DOMContentLoaded", () => {
   const botonFalso = document.querySelector(".addTicketButton");
